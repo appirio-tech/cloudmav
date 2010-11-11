4c3be5c0f4f1fdd17e6ecbc9e1cc3592fa41f50b
Jonathan Birkholz <jb@chaione.com>
2010-11-11 01:23:30 -0600

    bit bucket start

---
+++ lib/bit_bucket_service.rb
@@ -0,0 +1,35 @@
+require 'net/http'
+require 'net/https'
+require 'json'
+
+class BitBucketService
+  
+  def self.synch(profile)
+    # url = URI.parse("https://api.bitbucket.org/1.0/users/#{profile.username}")
+    # 
+    # response = Net::HTTP.get_response url
+    # result = JSON.parse(response.body)
+    
+    # Net::HTTP.start("https://api.bitbucket.org/1.0/users/") {|http|
+    #       req = Net::HTTP::Get.new("#{profile.username}")
+    #       req.basic_auth 'rookieone', 'pennyM00n'
+    #       response = http.request(req)
+    #       print response.body
+    #     }
+
+    url = URI.parse("https://api.bitbucket.org/1.0/users/#{profile.username}")
+    req = Net::HTTP::Get.new(url.path)
+    res = Net::HTTP.start(url.host, url.port) {|http|
+      req.basic_auth 'rookieone', 'pennyM00n'
+      response = http.request(req)
+      print response.body
+    }
+        
+    profile.url = "https://bitbucket.org/#{profile.username}"    
+    profile.repository_count = result["repositories"].count
+    followers_count = 0
+    result["repositories"].each{|r| followers_count += r["followers_count"].to_i }
+    profile.followers_count = followers_count
+  end
+  
+end
