5037df0d87a75de1bcbb14e60fc000758f160b1a
Jonathan Birkholz <jb@chaione.com>
2010-12-15 12:44:34 -0600

    add project on ui

--- app/helpers/stack_overflow_helper.rb
+++ app/helpers/stack_overflow_helper.rb
@@ -2,7 +2,7 @@ module StackOverflowHelper
   
   
   def stack_overflow_badges(profile)
-    return "" if profile.badge.nil?
+    return "" if profile.badge_html.nil?
     profile.badge_html.html_safe
   end
   
