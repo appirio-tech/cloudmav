9dffbfea26805b2601eede8be4827ed452793157
Jonathan Birkholz <rookieone@gmail.com>
2010-12-16 22:13:37 -0600

    changed tabs on profile

--- app/controllers/my/profiles_controller.rb
+++ app/controllers/my/profiles_controller.rb
@@ -1,9 +1,26 @@
 class My::ProfilesController < My::MyController
   before_filter :set_profile
   
-  def experience
+  def show
+    @tab = "summary"
   end
   
+  def code
+    @tab = "code"
+  end
+  
+  def experience
+    @tab = "experience"
+  end
+
+  def writing
+    @tab = "writing"
+  end
+  
+  def speaking
+    @tab = "speaking"
+  end
+    
   def update
     if @profile.update_attributes(params[:profile])
       flash[:notice] = "Profile updated"
