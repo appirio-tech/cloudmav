7d27cec73fc36802ad44cf2be015a1e85e501b1f
Jonathan Birkholz <rookieone@gmail.com>
2011-02-07 11:44:58 -0600

    updated talk added event

--- app/models/events/talk_added_event.rb
+++ app/models/events/talk_added_event.rb
@@ -3,9 +3,10 @@ class TalkAddedEvent < Event
   referenced_in :talk, :inverse_of => :events
 
   def perform
-    self.profile.award_badge("Yap yap yap", :description => "For having a talk")
-    self.profile.earn("for adding a talk", 10, :speaker_points) 
-    self.profile.save
+    profile.award_badge("Yap yap yap", :description => "For having a talk")
+    profile.earn("for adding a talk", 10, :speaker_points) 
+    talk.calculate_tags
+    profile.save
   end
 
   def set_category
