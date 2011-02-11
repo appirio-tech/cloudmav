175fcc43a3b30766d18493d721df02370a23769d
Jonathan Birkholz <rookieone@gmail.com>
2011-02-11 13:02:28 -0600

    moved writer module around and fix body css

--- lib/code_mav/writer_module.rb
+++ lib/code_mav/writer_module.rb
@@ -11,16 +11,6 @@ module CodeMav
     
     module InstanceMethods
       
-      def writer_tags
-        []
-      end
-      
-      # def blog_posts
-      #   posts = []
-      #   self.blogs.each { |b| posts.concat b.posts }
-      #   return posts.sort{|x,y| y.written_on <=> x.written_on }
-      # end
-      
     end
   end
 end
