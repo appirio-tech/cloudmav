ed6f27055f7421ae7c49fbaedeaf4168fdeae92c
Jonathan Birkholz <rookieone@gmail.com>
2011-05-25 11:57:37 -0500

    changed home page to backlog page if logged in

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,9 @@
 class PagesController < ApplicationController
   
   def home
+    if current_profile
+      redirect_to backlog_path
+    end
     @events = ProfileEvent.public.order_by(:date.desc).limit(10)
     @profile = Profile.showcase.first
   end
