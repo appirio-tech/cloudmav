819b6b01c783469d9f23d24e23768cd5f57c9005
Jonathan Birkholz <rookieone@gmail.com>
2011-08-13 10:48:27 -0500

    scottbs no repo bitbucket bug fix

--- app/models/events/bitbucket_profile_sync_event.rb
+++ app/models/events/bitbucket_profile_sync_event.rb
@@ -1,25 +1,12 @@
 class BitbucketProfileSyncEvent < SyncEvent
   referenced_in :bitbucket_profile, :inverse_of => :events
 
-  def find_or_initialize_repo(bb_repo)
-    repo = bitbucket_profile.repositories.where(:name => bb_repo["name"]).first
-    unless repo
-      repo = BitbucketRepository.new(:name => bb_repo["name"])
-      repo.bitbucket_profile = bitbucket_profile
-    end
-    repo
-  end
-
   def sync
     user_info = BitbucketApi.get_user(bitbucket_profile.username)
 
-    user_info["repositories"].each do |r|
-      repo = find_or_initialize_repo(r)
-      repo.url = r["url"]
-      repo.description = r["description"]
-      repo.followers_count = r["followers_count"]
-      repo.save
-    end
+    repositories = user_info["repositories"]
+
+    read_repositories(repositories) unless repositories.nil?
 
     bitbucket_profile.followers_count = bitbucket_profile.repositories.inject(0){|sum, r| sum + r.followers_count}
     bitbucket_profile.repository_count = bitbucket_profile.repositories.count
@@ -27,6 +14,25 @@ class BitbucketProfileSyncEvent < SyncEvent
     bitbucket_profile.save
 
     bitbucket_profile.retag!
+  end
+
+  def read_repositories(repositories)
+    repositories.each do |r|
+      repo = find_or_initialize_repo(r)
+      repo.url = r["url"]
+      repo.description = r["description"]
+      repo.followers_count = r["followers_count"]
+      repo.save
+    end
+  end
+
+  def find_or_initialize_repo(bb_repo)
+    repo = bitbucket_profile.repositories.where(:name => bb_repo["name"]).first
+    unless repo
+      repo = BitbucketRepository.new(:name => bb_repo["name"])
+      repo.bitbucket_profile = bitbucket_profile
+    end
+    repo
   end
 
 end
--- spec/models/events/bitbucket_profile_sync_event_spec.rb
+++ spec/models/events/bitbucket_profile_sync_event_spec.rb
@@ -24,6 +24,28 @@ describe "BitbucketProfileSyncEvent" do
     it { @profile.score(:coder_points).should == 10 }
   end
 
+  describe "scottburton 11 sync" do
+    before(:each) do
+      @profile = Factory.create(:user).profile
+      @bitbucket_profile = BitbucketProfile.new(:username => "scottburton11")
+      @profile.bitbucket_profile = @bitbucket_profile
+      @bitbucket_profile.save
+      @bitbucket_profile.expects(:retag!)
+      event = BitbucketProfileSyncEvent.new(:bitbucket_profile => @bitbucket_profile, :profile => @profile)
+
+      VCR.use_cassette("bitbucket_sync_event", :record => :new_episodes) do
+        event.sync
+      end
+      @profile = Profile.find(@profile.id)
+    end
+
+    it { @bitbucket_profile.url.should == "http://www.bitbucket.org/scottburton11" }
+    it { @bitbucket_profile.username.should_not be_empty }
+    it { @bitbucket_profile.repository_count.should == "0" }
+    it { @bitbucket_profile.followers_count.should == "0" }
+    it { @profile.score(:coder_points).should == 10 }
+  end
+
 end
 
 
