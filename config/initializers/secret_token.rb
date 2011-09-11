d7c3e7675df244c7e2fcca75a765fc68cfa30024
Jonathan Birkholz <jb@chaione.com>
2011-09-11 14:21:24 -0500

    rails_3_1 init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Codemav::Application.config.secret_token = '69abe5427251531f190baebfa9364e5f0fc22aee3e251c93ba4f01ce19813a33d2227d7701ccf35a189c1db6b67a99f06f9048d6160b9e584495b097acc87422'
