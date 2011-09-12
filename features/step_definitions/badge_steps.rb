cf189bbc0ca753ba46a07a998b174700edb90695
Jonathan Birkholz <jb@chaione.com>
2011-09-12 10:47:20 -0500

    profile reload on badge step

--- features/step_definitions/badge_steps.rb
+++ features/step_definitions/badge_steps.rb
@@ -1,4 +1,4 @@
 Then /^I should be awarded the "([^"]*)" badge$/ do |name|
-  profile = User.find(@user.id).profile
-  profile.has_badge_named?(name).should == true
+  @profile.reload
+  @profile.has_badge_named?(name).should == true
 end
