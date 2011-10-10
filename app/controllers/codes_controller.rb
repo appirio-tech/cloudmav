8801053390c76ef15919106d439848d596d6aaa1
Jonathan Birkholz <jb@chaione.com>
2011-10-09 23:03:01 -0500

    styled bitbucket repos

--- app/controllers/codes_controller.rb
+++ app/controllers/codes_controller.rb
@@ -3,9 +3,13 @@ class CodesController < ApplicationController
   
   def show
     @tab = "code"
+    
     @git_hub_profile = @profile.git_hub_profile
     @git_hub_repositories = @git_hub_profile.repositories.order_by_score.page(params[:page]).per(5)
+    
     @bitbucket_profile = @profile.bitbucket_profile
+    @bitbucket_repositories = @bitbucket_profile.repositories.page(params[:page]).per(5)
+    
     @coder_wall_profile = @profile.coder_wall_profile
   end
   
