0377ed4460c94225f7be02e22bdf29c39fff92ab
Jonathan Birkholz <jb@chaione.com>
2011-10-10 08:26:31 -0500

    check it code repo rpofile exists before reading repos

--- app/controllers/codes_controller.rb
+++ app/controllers/codes_controller.rb
@@ -5,10 +5,16 @@ class CodesController < ApplicationController
     @tab = "code"
     
     @git_hub_profile = @profile.git_hub_profile
-    @git_hub_repositories = @git_hub_profile.repositories.order_by_score.page(params[:page]).per(5)
+    @git_hub_repositories = []
+    if @git_hub_profile
+      @git_hub_repositories = @git_hub_profile.repositories.order_by_score.page(params[:page]).per(5)
+    end
     
     @bitbucket_profile = @profile.bitbucket_profile
-    @bitbucket_repositories = @bitbucket_profile.repositories.page(params[:page]).per(5)
+    @bitbucket_repositories = []
+    if @bitbucket_repositories
+      @bitbucket_repositories = @bitbucket_profile.repositories.page(params[:page]).per(5)
+    end
     
     @coder_wall_profile = @profile.coder_wall_profile
   end
