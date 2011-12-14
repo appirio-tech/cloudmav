ec1746fdc5e9d6b6fe505a79d1f0dc2031dd7c35
Jonathan Birkholz <jb@chaione.com>
2011-12-14 16:55:42 -0600

    what is stackoverflow

--- app/assets/javascripts/syncable.js
+++ app/assets/javascripts/syncable.js
@@ -2,10 +2,15 @@
 $(document).ready(function(){
   
   $(".sync_help").click(function() {
-    $(this).parents("li").find(".block-message").slideDown();
+    $(this).parents("li").find(".help").slideDown();
     return false;
   });
   
+  $(".sync_what_is").click(function() {
+    $(this).parents("li").find(".what_is").slideDown();
+    return false;
+  });
+    
   $(".close").click(function() {
     $(this).parent(".block-message").slideUp();
     return false;
